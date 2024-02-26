f8bb2f92b4e37ea6fc9972c5359922c2e5c49fbf
Andrés Echeverry <anfeipe10@gmail.com>
2024-02-26 15:55:22 -0500

    first commit

---
+++ precache-manifest.6494d2794611aab2290b25fcd24dea68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a2010ea9ef2b005a80a1bb99a9784d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ea1c6def6d753b38c2c",
+    "url": "./static/css/main.c313d470.chunk.css"
+  },
+  {
+    "revision": "d116bff202bd127087c2",
+    "url": "./static/js/2.3d7e9aeb.chunk.js"
+  },
+  {
+    "revision": "7e1dd654071f9ab54b736d1773d39b95",
+    "url": "./static/js/2.3d7e9aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea1c6def6d753b38c2c",
+    "url": "./static/js/main.5d35641a.chunk.js"
+  },
+  {
+    "revision": "9a8cd2b855a6c6335424",
+    "url": "./static/js/runtime-main.a62cf095.js"
+  },
+  {
+    "revision": "1f294befe5b82b33119f2a231db64178",
+    "url": "./static/media/MercuryDisplay-Semibold.1f294bef.ttf"
+  },
+  {
+    "revision": "7366b80cdc283bef8e9422485f1550fc",
+    "url": "./static/media/MercuryDisplay-Semibold.7366b80c.woff"
+  },
+  {
+    "revision": "b5ec68c13352f07ddb85855d86491508",
+    "url": "./static/media/MercuryDisplay-Semibold.b5ec68c1.svg"
+  },
+  {
+    "revision": "cef1774b83299fe8e2693a4607454e72",
+    "url": "./static/media/MercuryDisplay-Semibold.cef1774b.eot"
+  }
+]);
---
+++ precache-manifest.cb1a5de217eac115c9c9975bf634f16a.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04f399db590b4ec9eeb6b0334d6ff9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11ceb82d6e0e0a385bbe",
+    "url": "./static/css/main.2cecfde7.chunk.css"
+  },
+  {
+    "revision": "ff52c881febfc923f93c",
+    "url": "./static/js/2.4b36b902.chunk.js"
+  },
+  {
+    "revision": "b720df6d0d814785e5938ab335dc538d",
+    "url": "./static/js/2.4b36b902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ceb82d6e0e0a385bbe",
+    "url": "./static/js/main.eec47488.chunk.js"
+  },
+  {
+    "revision": "9a8cd2b855a6c6335424",
+    "url": "./static/js/runtime-main.a62cf095.js"
+  },
+  {
+    "revision": "1f294befe5b82b33119f2a231db64178",
+    "url": "./static/media/MercuryDisplay-Semibold.1f294bef.ttf"
+  },
+  {
+    "revision": "7366b80cdc283bef8e9422485f1550fc",
+    "url": "./static/media/MercuryDisplay-Semibold.7366b80c.woff"
+  },
+  {
+    "revision": "b5ec68c13352f07ddb85855d86491508",
+    "url": "./static/media/MercuryDisplay-Semibold.b5ec68c1.svg"
+  },
+  {
+    "revision": "cef1774b83299fe8e2693a4607454e72",
+    "url": "./static/media/MercuryDisplay-Semibold.cef1774b.eot"
+  },
+  {
+    "revision": "8140702940caf3ef8d373bb9c3f84e26",
+    "url": "./static/media/ambiente1.81407029.jpg"
+  },
+  {
+    "revision": "4ff82df5fdda258c4a3a8d65ebffb0d9",
+    "url": "./static/media/ambiente2.4ff82df5.jpg"
+  },
+  {
+    "revision": "f7f4e895190c308b610d0e92fe17dea6",
+    "url": "./static/media/ambiente3.f7f4e895.jpg"
+  },
+  {
+    "revision": "7e7e8f7efa4d051cb1305ded936a6eed",
+    "url": "./static/media/criterios_mapa_01_en.7e7e8f7e.jpg"
+  },
+  {
+    "revision": "3a58950d41bd5b68bdf81bd3b1e7e31d",
+    "url": "./static/media/criterios_mapa_01_es.3a58950d.jpg"
+  },
+  {
+    "revision": "b0e82164bfaa212ca76591bd03f53e4a",
+    "url": "./static/media/criterios_mapa_02_en.b0e82164.jpg"
+  },
+  {
+    "revision": "5ef292093ae0f471580cf938dcf681f6",
+    "url": "./static/media/criterios_mapa_02_es.5ef29209.jpg"
+  },
+  {
+    "revision": "815c1e6cc6aa8d4dc457768d66dbb1d3",
+    "url": "./static/media/criterios_mapa_03_en.815c1e6c.jpg"
+  },
+  {
+    "revision": "ae03652cc32f6399a4a8637fb05f5cf2",
+    "url": "./static/media/criterios_mapa_03_es.ae03652c.jpg"
+  },
+  {
+    "revision": "83fe72fab178d5e3e33596fe29ffa478",
+    "url": "./static/media/criterios_mapa_04_en.83fe72fa.jpg"
+  },
+  {
+    "revision": "fb821b33961523bba5c597904ad9421c",
+    "url": "./static/media/criterios_mapa_04_es.fb821b33.jpg"
+  },
+  {
+    "revision": "092dfc60ca84c3c3becf14f4cd80a2d3",
+    "url": "./static/media/impacto_polinizacion_A_en.092dfc60.jpg"
+  },
+  {
+    "revision": "f6111b2fc24100aa7ef23aa75b09072b",
+    "url": "./static/media/impacto_polinizacion_A_es.f6111b2f.jpg"
+  },
+  {
+    "revision": "775aded7216a25932ac5df4525e06d9c",
+    "url": "./static/media/impacto_polinizacion_B_en.775aded7.jpg"
+  },
+  {
+    "revision": "ebfcf7aa7b12f9da5ebccc9de6614eb5",
+    "url": "./static/media/impacto_polinizacion_B_es.ebfcf7aa.jpg"
+  },
+  {
+    "revision": "cd5e5a94637c2d08e870555b3ee244b2",
+    "url": "./static/media/impacto_semillas_A_en.cd5e5a94.jpg"
+  },
+  {
+    "revision": "8e883dc878b2ac8e81e0488998a4ff59",
+    "url": "./static/media/impacto_semillas_A_es.8e883dc8.jpg"
+  },
+  {
+    "revision": "72e7fbb635802b3784e1e7a4b572b913",
+    "url": "./static/media/impacto_semillas_B_en.72e7fbb6.jpg"
+  },
+  {
+    "revision": "ff997f069e66655d5836797d05df8e83",
+    "url": "./static/media/impacto_semillas_B_es.ff997f06.jpg"
+  },
+  {
+    "revision": "ff02c0e246116d9efd8f2c51940143e0",
+    "url": "./static/media/perdida_ecosistema_01_en.ff02c0e2.jpg"
+  },
+  {
+    "revision": "171dca153775394481e041e0642f84c0",
+    "url": "./static/media/perdida_ecosistema_01_es.171dca15.jpg"
+  },
+  {
+    "revision": "2025521c57a0e0dacea9e5334e52ecdd",
+    "url": "./static/media/perdida_ecosistema_02_en.2025521c.jpg"
+  },
+  {
+    "revision": "2b9142c0e2e41f79594dbf0fec41441a",
+    "url": "./static/media/perdida_ecosistema_02_es.2b9142c0.jpg"
+  },
+  {
+    "revision": "57544c06fba37192d03c806c3f5cb7e7",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_01_en.57544c06.jpg"
+  },
+  {
+    "revision": "fa0bd2fc89565c35f414be101687fc61",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_01_es.fa0bd2fc.jpg"
+  },
+  {
+    "revision": "500f500c8d39b1a063f7d1be29027755",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_02_en.500f500c.jpg"
+  },
+  {
+    "revision": "b56cfcbf4a2eacefe069cc634748f1f9",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_02_es.b56cfcbf.jpg"
+  },
+  {
+    "revision": "eaf25d02548abb5c6e52978d66513587",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_03_en.eaf25d02.jpg"
+  },
+  {
+    "revision": "3ea3a11c342136036d83388f5f47cdad",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_03_es.3ea3a11c.jpg"
+  },
+  {
+    "revision": "6812b265634cd098734347e26ee671a2",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_04_en.6812b265.jpg"
+  },
+  {
+    "revision": "a3a639f8a55ff6a24d26b2961f608105",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_04_es.a3a639f8.jpg"
+  },
+  {
+    "revision": "75ff90125f8b05351079a054082e61a3",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_05_en.75ff9012.jpg"
+  },
+  {
+    "revision": "f4fe918bd8e2cf7b19ebf9db5cc36b4a",
+    "url": "./static/media/perdida_ecosistema_actividad_humana_05_es.f4fe918b.jpg"
+  },
+  {
+    "revision": "085f21bad02211f80867184c8932bbfe",
+    "url": "./static/media/restauracion_ecosistemas_02_en.085f21ba.jpg"
+  },
+  {
+    "revision": "577cd2366cfc1cd54931a111d051dda1",
+    "url": "./static/media/restauracion_ecosistemas_02_es.577cd236.jpg"
+  },
+  {
+    "revision": "0c2d09d892635f0cf898adcfb5b4d9d6",
+    "url": "./static/media/restauracion_ecosistemas_en.0c2d09d8.jpg"
+  },
+  {
+    "revision": "b89f6e07c656ff471c375a879b7be192",
+    "url": "./static/media/restauracion_ecosistemas_es.b89f6e07.jpg"
+  }
+]);
